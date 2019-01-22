6266cc7d050bdc490bff5a75f3ca75adefdeb7dd
Hayden Free <hayden.free@uky.edu>
2019-01-22 18:03:06 -0500

    Update bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -1,3 +1,3 @@
 # Create a source bundle for Elastic Beanstalk
 # The zip is placed in the parent of the current folder
-git archive -v -o website.zip --format=zip HEAD
+git archive -v -o ../website.zip --format=zip HEAD
