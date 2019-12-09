c6ae2897d561fe5530cbccbaa72e1278781c054c
Hayden Free <hayden.free@uky.edu>
2019-12-09 15:06:57 -0500

    Update composer and RegistryInterface to ManagerRegistry

--- src/Repository/TeamRepository.php
+++ src/Repository/TeamRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\Team;
 use App\Entity\TeamMember;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Team|null find($id, $lockMode = null, $lockVersion = null)
@@ -15,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class TeamRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Team::class);
     }
