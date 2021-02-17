958655ffaadf8028129dcef940c2e9f0113aa2e7
Nilpatel934 <nilpatel934@gmail.com>
2021-02-17 18:12:38 -0500

    Doctrine fix

--- src/Repository/TrackablePageRepository.php
+++ src/Repository/TrackablePageRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\TrackablePage;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method TrackablePage|null find($id, $lockMode = null, $lockVersion = null)
