98bbd8c346d7d558579e8747ffa196717defcd58
Fireball021419 <asjo237@uky.edu>
2022-02-25 09:11:16 -0500

    xdxde

--- src/Migrations/Version20180817181303.php
+++ src/Migrations/Version20180817181303.php
@@ -13,7 +13,7 @@ final class Version20180817181303 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->name !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE team ADD short_description VARCHAR(280) NOT NULL, ADD long_description VARCHAR(1024) NOT NULL, ADD hero_url VARCHAR(255) DEFAULT NULL, ADD hero_image VARCHAR(255) DEFAULT NULL');
     }
@@ -21,7 +21,7 @@ final class Version20180817181303 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->name !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE team DROP short_description, DROP long_description, DROP hero_url, DROP hero_image');
     }
