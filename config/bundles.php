c32afc8ebba9a6dbdf1c6b28d8a3c7295d2f4de7
Fireball021419 <asjo237@uky.edu>
2022-03-09 15:17:49 -0500

    bjkhbkjhvb

--- config/bundles.php
+++ config/bundles.php
@@ -11,6 +11,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
